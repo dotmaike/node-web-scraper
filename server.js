95eeb761fe5c2124cce9f857512ba2b136477d8e
Miguel Murillo <miguel_murillo@epam.com>
2017-04-26 01:59:35 -0500

    Use async to obtain pagination

--- server.js
+++ server.js
@@ -1,45 +1,109 @@
 var express = require('express');
-var fs      = require('fs');
-var request = require('request');
+var fs = require('fs');
+var async = require("async");
+var request = require("request");
+var rp = require('request-promise');
 var cheerio = require('cheerio');
-var app     = express();
+var app = express();
 
-app.get('/scrape', function(req, res){
-  // Let's scrape Anchorman 2
-  url = 'http://www.imdb.com/title/tt1229340/';
+app.get('/scrape', function(req, res) {
+    /*var requests = [];
+    for (var page = 1; page < 11; page++) {
+        var options = {
+            uri: 'https://www.promodescuentos.com/?page=' + page,
+            transform: function(body) {
+                return cheerio.load(body);
+            }
+        };
+        requests.push(options);
+    }*/
 
-  request(url, function(error, response, html){
-    if(!error){
-      var $ = cheerio.load(html);
-
-      var title, release, rating;
-      var json = { title : "", release : "", rating : ""};
-
-      $('.title_wrapper').filter(function(){
-        var data = $(this);
-        title = data.children().first().text().trim();
-        release = data.children().last().children().last().text().trim();
-
-        json.title = title;
-        json.release = release;
-      })
-
-      $('.ratingValue').filter(function(){
-        var data = $(this);
-        rating = data.text().trim();
-
-        json.rating = rating;
-      })
+    var requests = [];
+    for (var page = 1; page < 11; page++) {
+        requests.push({
+            url: 'https://www.promodescuentos.com/?page=' + page,
+        });
     }
 
-    fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err){
-      console.log('File successfully written! - Check your project directory for the output.json file');
-    })
+    async.map(requests, function(obj) {
+        /*rp(obj)
+            .then(function($) {
+                var title, category, link;
+                var json = {};
 
-    res.send('Check your console!')
-  })
+                $('.thread-content').filter(function(i, el) {
+                    var data = $(this);
+                    category = data.children().children('.mute--text').text();
+                    title = data.children().children().text();
+                    link = data.children().children().find($('.vwo-deal-button')).attr('href');
+
+                    if (json.hasOwnProperty(category)) {
+                        json[category].push({
+                            title: title,
+                            link: link
+                        });
+                    } else {
+                        json[category] = [];
+                        json[category].push({
+                            title: title,
+                            link: link
+                        });
+                    }
+                });
+
+                fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err) {
+                    console.log('File successfully written! - Check your project directory for the output.json file');
+                });
+
+                res.send(json);
+            })
+            .catch(function(err) {
+                console.log(err);
+            });*/
+        request(obj.url, function(error, response, html) {
+            if (!error) {
+                var $ = cheerio.load(html);
+
+                var title, category, link;
+                var json = {};
+
+                $('.thread-content').filter(function(i, el) {
+                    var data = $(this);
+                    category = data.children().children('.mute--text').text();
+                    title = data.children().children().text();
+                    link = data.children().children().find($('.vwo-deal-button')).attr('href');
+
+                    if (json.hasOwnProperty(category)) {
+                        json[category].push({
+                            title: title,
+                            link: link
+                        });
+                    } else {
+                        json[category] = [];
+                        json[category].push({
+                            title: title,
+                            link: link
+                        });
+                    }
+                });
+            }
+
+            fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err) {
+                console.log('File successfully written! - Check your project directory for the output.json file');
+            });
+
+            res.send(json);
+        })
+    }, function(err, results) {
+        // all requests have been made
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(results);
+        }
+    });
 })
 
-app.listen('8081')
-console.log('Magic happens on port 8081');
+app.listen('6969')
+console.log('Magic happens on port 6969');
 exports = module.exports = app;
